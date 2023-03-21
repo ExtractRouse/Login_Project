25c02e7aa8e89cc9db43f2633af7b31161c5fd5c
AlexeyIzhbulatov <ouren@mail.ru>
2023-03-21 17:41:01 +0300

    steps 3-registration(fulfilled)

--- wizard.js
+++ wizard.js
@@ -1,13 +1,18 @@
 let d = document.querySelector('.button-block > .button-block__registration');
 let c = document.querySelector('.blockStep-One');
 let b = document.querySelector('.steps');
+let buttonNext = document.querySelector('.button-block__next-width');
+let stepThreeBlock = document.querySelector('.stepThree');
 
-console.log(c);
-console.log(y);
+
 
 
 d.onclick = (e) => {
    e.preventDefault();
    c.style.display = 'none';
    b.style.display = 'block'
+}
+buttonNext.onclick = (e) => {
+   b.style.display = 'none';
+   stepThreeBlock.style.display = 'block';
 }
