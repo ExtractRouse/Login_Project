9973874c4577d10aab33f1e079d015fb26b95745
AlexeyIzhbulatov <ouren@mail.ru>
2023-03-23 23:16:33 +0300

    steps 2-registration, btn-pagination(fulfilled)

--- wizard.js
+++ wizard.js
@@ -3,6 +3,8 @@ let c = document.querySelector('.blockStep-One');
 let b = document.querySelector('.steps');
 let buttonNext = document.querySelector('.button-block__next-width');
 let stepThreeBlock = document.querySelector('.stepThree');
+let paginationBtnLeft = document.querySelector('#btn-left');
+let paginationBtnRight = document.querySelector('#btn-right');
 
 
 
@@ -15,4 +17,31 @@ d.onclick = (e) => {
 buttonNext.onclick = (e) => {
    b.style.display = 'none';
    stepThreeBlock.style.display = 'block';
+};
+
+paginationBtnLeft.onclick = (e) => {
+   paginationBtnLeft.style.width = '16px';
+
+   paginationBtnLeft.style.height = '7px';
+   paginationBtnLeft.style.backgroundColor = '#52c3ff';
+   paginationBtnLeft.style.borderRadius = '5px';
+
+   paginationBtnRight.style.width = "6px";
+   paginationBtnRight.style.height = "7px";
+   paginationBtnRight.style.backgroundColor = "gray";
+   paginationBtnRight.style.borderRadius = "5px";
+
+}
+
+paginationBtnRight.onclick = (e) => {
+   paginationBtnLeft.style.width = '6px';
+   paginationBtnLeft.style.height = '7px';
+   paginationBtnLeft.style.backgroundColor = '#52c3ff';
+   paginationBtnLeft.style.borderRadius = '5px';
+   paginationBtnRight.style.width = '16px';
+
+   paginationBtnRight.style.height = '7px';
+   paginationBtnRight.style.backgroundColor = '#52c3ff';
+   paginationBtnRight.style.borderRadius = '5px';
+   paginationBtnLeft.style.backgroundColor = 'gray';
 }
